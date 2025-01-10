ee722d6954d7b34b6099ca00b8a9c2ce93fc27ef
Radiant Fadilah <radiantfadilah0@gmail.com>
2025-01-10 15:19:48 +0700

    slicing settings item to component

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,46 +1,13 @@
-import { StyleSheet, View } from "react-native";
-import { Picker } from "@react-native-picker/picker";
-import { useAppDispatch, useAppSelector } from "@/hooks";
-import { fonts } from "@/constant";
-import { setAppFont } from "@/redux/slices/app-settings";
-import { Gap, Text } from "@/components";
+import { View } from "react-native";
+import { AppFont, AppTheme } from "@/components/settings";
+import { Gap } from "@/components";
 
 export default function Settings() {
-  const dispatch = useAppDispatch();
-  const { app_font } = useAppSelector((state) => state.settings);
-
   return (
     <View style={{ padding: 20 }}>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Text>App Font</Text>
-        <View style={styles.line} />
-      </View>
-      <Picker
-        mode="dropdown"
-        selectedValue={app_font}
-        onValueChange={(selectedFont) => dispatch(setAppFont(selectedFont))}
-      >
-        {fonts.map((font) => (
-          <Picker.Item label={font.name} value={font.name} key={font.name} />
-        ))}
-      </Picker>
-      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <Text style={{ fontWeight: "bold" }}>Bold</Text>
-        <Text>Regular</Text>
-        <Text style={{ fontWeight: "thin" }}>Thin</Text>
-        <Text style={{ fontStyle: "italic" }}>Italic</Text>
-      </View>
-      <Gap height={10} />
-      <View style={{ ...styles.line, flex: 0, width: "100%", marginLeft: 0 }} />
+      <AppTheme />
+      <Gap height={20} />
+      <AppFont />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  line: {
-    flex: 1,
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: "black",
-    marginLeft: 20,
-  },
-});
