d13a1abb18233a2d54d408cf3074b291fd9b204b
Radiant Fadilah <radiantfadilah0@gmail.com>
2025-01-10 10:27:46 +0700

    initial commit

---
+++ app.config.js
@@ -0,0 +1,79 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return "com.exkoi.dev.TaskManager.dev";
+  }
+
+  if (IS_PREVIEW) {
+    return "com.exkoi.dev.TaskManager.preview";
+  }
+
+  return "com.exkoi.dev.TaskManager";
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return "Task Manager (Dev)";
+  }
+
+  if (IS_PREVIEW) {
+    return "Task Manager (Preview)";
+  }
+
+  return "Task Manager: List Your Need";
+};
+
+export default {
+  expo: {
+    name: getAppName(),
+    slug: "TaskManager",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: getUniqueIdentifier(),
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: getUniqueIdentifier(),
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: "fe2982f0-e3d7-4127-a203-bff07f825f65",
+      },
+    },
+    owner: "exkoi.dev",
+  },
+};
