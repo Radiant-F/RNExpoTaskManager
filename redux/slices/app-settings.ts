996f4cc7c56e541c9326fdd533379a1f791545a2
Radiant Fadilah <radiantfadilah0@gmail.com>
2025-01-10 18:10:10 +0700

    add app theme

--- redux/slices/app-settings.ts
+++ redux/slices/app-settings.ts
@@ -1,12 +1,15 @@
 import { FontType } from "@/constant";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Appearance, ColorSchemeName } from "react-native";
 
 type Props = {
   app_font: FontType;
+  app_theme: ColorSchemeName;
 };
 
 const initialState: Props = {
   app_font: "System Default",
+  app_theme: Appearance.getColorScheme(),
 };
 
 export const settingsSlice = createSlice({
@@ -16,9 +19,12 @@ export const settingsSlice = createSlice({
     setAppFont(state, action: PayloadAction<Props["app_font"]>) {
       state.app_font = action.payload;
     },
+    setAppTheme(state, action: PayloadAction<Props["app_theme"]>) {
+      state.app_theme = action.payload;
+    },
   },
 });
 
-export const { setAppFont } = settingsSlice.actions;
+export const { setAppFont, setAppTheme } = settingsSlice.actions;
 
 export default settingsSlice.reducer;
